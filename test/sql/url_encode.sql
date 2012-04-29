476280246c2fb5520842e0c16463d6e0a13a6110
Pavel Stehule <pavel.stehule@gmail.com>
2012-04-29 16:51:22 +0200

    fix versioning for pgxn

--- test/sql/url_encode.sql
+++ test/sql/url_encode.sql
@@ -2,3 +2,5 @@ CREATE EXTENSION url_encode;
 
 SELECT url_encode('Hello World');
 SELECT url_decode('Hello%20World');
+
+SELECT url_encode(unnest) from unnest(string_to_array('http://hu.wikipedia.org/wiki/São_Paulo','/'));
