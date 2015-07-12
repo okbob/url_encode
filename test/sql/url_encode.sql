b3f19406524dc3d8f867bb00a9034799f2c47122
Pavel Stehule <pavel.stehule@gooddata.com>
2015-07-12 14:32:17 +0200

    regress test stabilization - remove utf8 output

--- test/sql/url_encode.sql
+++ test/sql/url_encode.sql
@@ -6,4 +6,4 @@ SELECT url_decode('Hello%20World');
 SELECT url_encode(unnest) from unnest(string_to_array('http://hu.wikipedia.org/wiki/São_Paulo','/'));
 
 SELECT uri_encode('http://hu.wikipedia.org/wiki/São_Paulo');
-SELECT uri_decode('http://hu.wikipedia.org/wiki/S%C3%A3o_Paulo');
+SELECT md5(uri_decode('http://hu.wikipedia.org/wiki/S%C3%A3o_Paulo'));
