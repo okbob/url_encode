5bacd372e843c73e3fdaa7e020e9d76cd891eeca
Pavel Stehule <pavel.stehule@gooddata.com>
2017-08-17 08:22:27 +0200

    #5 decode percent encoded utf16 chars

--- src/url_encode.c
+++ src/url_encode.c
@@ -4,10 +4,10 @@
  *	  PostgreSQL functions for url code support
  *
  * Author:	Pavel Stehule
- * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
+ * Postcardware licence @2017
  *
  * IDENTIFICATION
- *	  contrib/url_encode/url_encode.c
+ *	  url_encode/url_encode.c
  *
  *-------------------------------------------------------------------------
  */
@@ -120,6 +120,21 @@ encode(text *in_text, const char *unreserved_special)
 	return result;
 }
 
+static uint32_t
+decode_utf16_pair(uint16_t c1, uint16_t c2)
+{
+	uint32_t code;
+
+	Assert(0xD800 <= c1 && c1 <= 0xDBFF);
+	Assert(0xDC00 <= c2 && c2 <= 0xDFFF);
+
+	code = 0x10000;
+	code += (c1 & 0x03FF) << 10;
+	code += (c2 & 0x03FF);
+
+	return code;
+}
+
 static text *
 decode(text *in_text, const char *unreserved_special)
 {
@@ -131,6 +146,7 @@ decode(text *in_text, const char *unreserved_special)
 	int		processed;
 
 	real_len = 0;
+
 	result = (text *) palloc(len + VARHDRSZ);
 	write_ptr = VARDATA(result);
 
@@ -156,14 +172,72 @@ decode(text *in_text, const char *unreserved_special)
 		}
 		else
 		{
-			/* next two chars should be utf8 digits */
-			if (processed + 3 > len)
+			if (processed + 1 >= len)
 				elog(ERROR, "incomplete input string");
 
-			*((unsigned char *) write_ptr++) = (get_hex(read_ptr[1]) << 4) | get_hex(read_ptr[2]);
-			real_len += 1;
-			processed += 3;
-			read_ptr += 3;
+			/* next two/four chars are part of UTF16 char */
+			if (read_ptr[1] == 'u' || read_ptr[1] == 'U')
+			{
+				unsigned char	b1;
+				unsigned char	b2;
+				uint32_t	u;
+				unsigned char	buffer[10];
+				int		utf8len;
+
+				uint16_t c1;
+				uint16_t c2;
+
+				/* read first two bytes */
+				if (processed + 5 > len)
+					elog(ERROR, "incomplete input string");
+
+				b1 = (get_hex(read_ptr[2]) << 4) | get_hex(read_ptr[3]);
+				b2 = (get_hex(read_ptr[4]) << 4) | get_hex(read_ptr[5]);
+				c1 = b2 | (b1 << 8);
+
+				if (0xD800 <= c1 && c1 <= 0xDBFF)
+				{
+					if (processed + 9 > len)
+						elog(ERROR, "incomplete input string");
+
+					b1 = (get_hex(read_ptr[6]) << 4) | get_hex(read_ptr[7]);
+					b2 = (get_hex(read_ptr[8]) << 4) | get_hex(read_ptr[9]);
+					c2 = b2 | (b1 << 8);
+
+					if (!(0xDC00 <= c2 && c2 <= 0xDFFF))
+						elog(ERROR, "invalid utf16 input char");
+
+					u = decode_utf16_pair(c1, c2);
+					processed += 10;
+					read_ptr += 10;
+				}
+				else
+				{
+					u = c1;
+					processed += 6;
+					read_ptr += 6;
+				}
+
+				unicode_to_utf8((pg_wchar) u, buffer);
+				utf8len = pg_utf_mblen(buffer);
+				strncpy(write_ptr, (const char *) buffer, utf8len);
+				write_ptr += utf8len;
+				real_len += utf8len;
+			}
+			else
+			{
+				/*
+				 * next two/three chars are part of UTF8 char, but it can
+				 * be decoded byte by byte.
+				 */
+				if (processed + 3 > len)
+					elog(ERROR, "incomplete input string");
+
+				*((unsigned char *) write_ptr++) = (get_hex(read_ptr[1]) << 4) | get_hex(read_ptr[2]);
+				real_len += 1;
+				processed += 3;
+				read_ptr += 3;
+			}
 		}
 	}
 
--- test/sql/url_encode.sql
+++ test/sql/url_encode.sql
@@ -7,3 +7,5 @@ SELECT url_encode(unnest) from unnest(string_to_array('http://hu.wikipedia.org/w
 
 SELECT uri_encode('http://hu.wikipedia.org/wiki/São_Paulo');
 SELECT md5(uri_decode('http://hu.wikipedia.org/wiki/S%C3%A3o_Paulo'));
+
+SELECT url_decode('%u6D6A%u82B1%u4E00%u6735%u6735%20%u7B2C8%u96C6%20-%20%u89C6%u9891%u5728%u7EBF%u89C2%u770B%20-%20%u6D6A%u82B1%u4E00%u6735%u6735%20-%20%u8292%u679CTV');
